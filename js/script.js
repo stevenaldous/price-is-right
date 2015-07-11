998c18042e1056bdf55ae2977d9cd51020b77faa
stevenaldous <stevenaldous@gmail.com>
2015-07-10 22:49:28 -0700

    filled in more script, still need unction to assign points and determine winner

--- js/script.js
+++ js/script.js
@@ -1,23 +1,22 @@
 $(function(){
 	//button vars
-
-	var playBtn = $('#play-button');
-	var plyr1Btn = $('#plyr1-btn');
-	var plyr2Btn = $('#plyr2-btn');
-	var nextBtn = $('#next-button');
+	var newGame = $('#new-game')
+	var goBtn = $('#go-button');
 	var prodTitle = $('#product-name');
 	var showcase= $('#product-image');
 	var reveal = $('#product-price');
 	var roundCount = -1;
 
-	//hide next button and reveal box
+	//scoreCount
+	var plyr1Score= $('#1-score');
+	var plyr2Score= $('#2-score');
+	var score1 = '';
+	var score2 = '';
+
+
+	//hide reveal box
 	$('#reveal').hide();
-	$(nextBtn).hide();
-
-	//scorecount
-	var plyr1= '';
-	var plyr2='';
-
+	
 
 
 //objects in an array to pull at random
@@ -28,72 +27,76 @@ $(function(){
 			{photo: 'http://i.imgur.com/FkiAIW0.jpg', desc:'Philips Norelco 6945XL Electric Razor' ,price: 39.99},
 			{photo: 'http://i.imgur.com/s4bN9Oy.jpg', desc:'Bumble and Bumble The Surf Set Travel Size' ,price: 53.99},
 			{photo: 'http://i.imgur.com/7Qxz01m.jpg', desc:'Brooks England B17 Special Saddle' ,price: 171},
-			];
-
-	//array of random nums to check for duplicats		
-	indexPulled = [];
-
-
-
-	//grab from data at random
-	var randomNum=Math.floor((Math.random()*data.length)+1);
-	//check if random num has been generated
-	//returns boolean
-	var isUniqueIndex=function(num,array){
-		for(i=0;i=array.length;i++){
-			if(array[i] === num){
-				return true;
-			}else {
-				return false;
-			};
-		};
+			];		
+	
+	//array shuffle function
+	function shuffle(o) {
+    	for(var j, x, i =data.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    	return o;
+	};
+ //post product to page to showcase
+		
+	var postItAll = function(num){
+		showcase.append('<img src="'+data[num].photo+'">');
+		prodTitle.append('<p>'+data[num].desc+'</p>');
+		reveal.append('<p>$'+data[num].price+'</p>');
 	};
 
-	// //postimage to showcase
-	// var i=randomNum;
+	//restrict letters from input
+$('.form-control').keydown(function(event){
+	    if ( jQuery.inArray(event.keyCode,[46,8,9,27,13,190]) !== -1 ||
+            (event.keyCode >= 35 && event.keyCode <= 39)) {
+                 return;
+        } else {
+            if (event.shiftKey || (event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+                event.preventDefault(); 
+               }   
+        }
+})
 
-	// var images = data[i].photo;
-	// var postImage = function(images){
-	// 	showcase.append('<img src="'+images+'">')
-	// };
-	// //post title to product-name
-	// var toPost = data[i].desc;
-	// var postTitle = function(toPost){
-	// 	prodTitle.append('<p>'+toPost+'</p>');
-	// };
-	// //post price to reveal
-	// var truPrice = data[i].price;
-	// var postPrice = function(truPrice){
-	// 	reveal.append('<p>$'+truPrice+'</p>');
-	// };
 
 //game Start!
-	$(playBtn).on('click',function(e){
+	$(newGame).on('click',function(e){
 		e.preventDefault();
+		shuffle(data);
 		roundCount++;
 		plyr1=0;
 		plyr2=0;
-		$('#reveal').hide();
-		$(nextBtn).hide();	
+		postItAll(roundCount);
+		
+
 	}); //end of play button
 
-	//take and eval player inputs, assign pointo round winner
-	$(plyr1Btn).on('click',function(){
-		alert('Muhahahaha, its aliiiiiiiive!')
-	});
 
-	$(plyr2Btn).on('click',function(){
 	
-	})
-
-
+	//take and eval player inputs, assign pointo round winner
+	$('#swap').on('click', '#go-button', function(e){
+		e.preventDefault();
+		if($('#plyr1Input').val()==='' || $('#plyr2Input').val() === ''){
+			alert('need more than that!')
+		} else {
+		$('#swap').empty();
+		$('#swap').append('<button id="next-button" class="btn btn-warning" type="button">Next!</button>');
+		$('#reveal').show();
+		roundCount++;
 	//Next click action, remove prev product and place new one up.
+		$('#swap').on('click','#next-button',function(e){
+			e.preventDefault();
+			$(prodTitle).empty();
+			$(showcase).empty();
+			$(reveal).empty();
+			postItAll(roundCount);
+			$(plyr1Price).val(undefined);
+			$(plyr2Price).val(undefined);
+			$('#swap').empty();
+			$('#swap').append('<button id="go-button" class="btn btn-danger" type="button">GO!</button>');
+			$('#reveal').hide();
 
-
-
-	//determine winner and prompt for rematch
-
-
+			
+			});
+		};
+	});
+	
 
 
 
